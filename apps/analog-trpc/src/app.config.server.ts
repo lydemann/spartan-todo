e36577a5fd9a90357e6a76065e865b4adc8e30d9
Christian Lüdemann <christian@christianlydemann.com>
2023-04-29 17:24:33 +0200

    feat: set up basic app

---
+++ apps/analog-trpc/src/app.config.server.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, mergeApplicationConfig } from "@angular/core";
+import { provideServerRendering } from "@angular/platform-server";
+import { appConfig } from "./app.config";
+import { provideClientHydration } from "@angular/platform-browser";
+
+const serverConfig: ApplicationConfig = {
+  providers: [
+    provideServerRendering(),
+    provideClientHydration(),
+  ]
+};
+
+export const config = mergeApplicationConfig(appConfig, serverConfig);
